4adcdc14055fdee62ef2e103fb1733674f91d3f0
Carlos Hernández <learner.cys@gmail.com>
2015-08-30 23:28:41 -0600

    - create initial packages to scanners and parsers. - add menu items. - ignoring files.

--- src/main/java/net/project/Main.java
+++ src/main/java/net/project/Main.java
@@ -3,7 +3,7 @@ package net.project;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -12,8 +12,12 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane root = FXMLLoader.load(getClass().getResource("fxml/mainctrl.fxml"));
+        BorderPane root = FXMLLoader.load(getClass().getResource("fxml/mainctrl.fxml"));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    public static void main(String [] args) {
+        launch(args);
     }
 }
--- src/main/java/net/project/controllers/MainCtrl.java
+++ src/main/java/net/project/controllers/MainCtrl.java
@@ -4,4 +4,48 @@ package net.project.controllers;
  * @author learnercys on 30/08/15.
  */
 public class MainCtrl {
+
+    public void closeApplication() {
+
+    }
+
+    public void executeGame() {
+
+    }
+
+    public void showErrors() {
+
+    }
+
+    public void showSymbolsTable() {
+
+    }
+
+    public void newFile() {
+
+    }
+
+    public void onCompileStructure() {
+
+    }
+
+    public void onCompileScenarios() {
+
+    }
+
+    public void onCompileSecuences() {
+
+    }
+
+    public void openFile () {
+
+    }
+
+    public void saveFile() {
+
+    }
+
+    public void saveFileAs() {
+
+    }
 }
