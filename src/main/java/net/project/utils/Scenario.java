f4353d0abb4dbb32737c34cac58bd7c0048de8e8
Carlos Hernández <learner.cys@gmail.com>
2015-09-18 10:18:52 -0600

    Sequences - save scenarios sequences.

--- src/main/java/net/project/utils/Scenario.java
+++ src/main/java/net/project/utils/Scenario.java
@@ -4,9 +4,10 @@ package net.project.utils;
  * @author learnercys on 9/17/15.
  */
 public class Scenario {
+    
     private  String name;
-    private String bg;
     private Integer height;
     private Integer width;
+    private String bg;
 
 }
