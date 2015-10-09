c6cf580875f1eb25d164f6c3e2346bb1b59f76b6
Carlos Hernández <learner.cys@gmail.com>
2015-10-09 04:22:53 -0600

    Show sequences.

--- src/main/java/net/project/components/SequencesContainer.java
+++ src/main/java/net/project/components/SequencesContainer.java
@@ -1,8 +1,13 @@
 package net.project.components;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import java.util.ArrayList;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import net.project.utils.GenericElement;
 
 import java.io.IOException;
@@ -11,7 +16,10 @@ import java.io.IOException;
  * @author learnercys on 7/10/15.
  */
 public class SequencesContainer extends BorderPane{
+
     public ArrayList<GenericElement> sequences = new ArrayList<>();
+
+    @FXML VBox sequencesContainer;
 
     public SequencesContainer() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sequencescontainer.fxml"));
@@ -27,7 +35,17 @@ public class SequencesContainer extends BorderPane{
 
     public void setSequences(ArrayList<GenericElement> sequences) {
         this.sequences = sequences;
+        showSequences();
+    }
 
-        // todo show the sequences
+    private void showSequences() {
+        sequencesContainer.getChildren().removeAll();
+
+        for(GenericElement sequence: this.sequences){
+            HBox sContainer = new HBox();
+            sContainer.setAlignment(Pos.CENTER);
+            sContainer.getChildren().addAll(new Label("Nombre:" + sequence.getAttr("name").getValue()), new Label(", Orden: " + sequence.getAttr("order").getValue().toString()));
+            sequencesContainer.getChildren().add(sContainer);
+        }
     }
 }
